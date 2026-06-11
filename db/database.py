3c84aa26dbc87746f8a0c9c7e9c2ab830489273a
robin2728 <robinsingh2728@gamil.com>
2026-06-11 14:23:01 +0530

    Added SSH

--- db/database.py
+++ db/database.py
@@ -8,15 +8,15 @@ async def create_db_pool():
     global pool
 
     pool = await asyncpg.create_pool(
-        host=DB_CONFIG["host"],
-        database=DB_CONFIG["database"],
-        user=DB_CONFIG["user"],
-        password=DB_CONFIG["password"],
-        port=DB_CONFIG["port"],
-        min_size=5,
-        max_size=20
-    )
-
+    host=DB_CONFIG["host"],
+    database=DB_CONFIG["database"],
+    user=DB_CONFIG["user"],
+    password=DB_CONFIG["password"],
+    port=int(DB_CONFIG["port"]),
+    ssl="require",
+    min_size=5,
+    max_size=20
+)
 async def execute_sql(sql_query: str):
 
     async with pool.acquire() as connection:
